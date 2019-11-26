e8c04bc5070a015bcdaf0293c0919a27e870584a
Chyanne Haugen <42098386+crhaugen@users.noreply.github.com>
2019-11-25 20:07:03 -0800

    created findMarkers and findBounds

--- bookFindingTest.cpp
+++ bookFindingTest.cpp
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <vector>
 #include <cmath>
+#include <algorithm> 
+
 
 using namespace cv;
 using namespace std;
@@ -119,46 +121,150 @@ bool isSolidColor(const Mat& input) {
 	}
 	// divide the sum by the total number of pixels
 	total = total / (input.rows * input.cols);
-	if (total > .6)
+	if (total > .85)
 		return true;
+	return false;
+}
+
+/*
+* findBounds:
+*	Function tries to approximative a good threshold to use for finding 
+*		edges in any image. Does this based on how light/dark the grayscale image is
+*		Preconditions: meanColor contains the mean value for a given image
+*		Postconditions: lowerThreshold and upperThreshold contain calculated values.
+* 
+*/
+void findBounds(double meanColor, int &lowerThreshold, int &upperThreshold)
+{
+	double sigma = 0.33;
+	if (meanColor > 191) 
+	{
+		lowerThreshold = (int)max(double(0), (1 - 2 * sigma) * (255 - meanColor));
+		upperThreshold = (int)max(double(85), (1 + 2 * sigma) * (255 - meanColor));
+	}
+	else if (meanColor > 127) 
+	{
+		lowerThreshold = (int)max(double(0), (1 - sigma) * (255 - meanColor));
+		upperThreshold = (int)min(double(255), (1 + sigma) * (255 - meanColor));
+	}
+	else if (meanColor < 63) 
+	{
+		lowerThreshold = (int)max(double(0), (1 - 2 * sigma) * meanColor);
+		upperThreshold = (int)max(double(85), (1 + 2 * sigma) * meanColor);
+	}
+	else
+	{
+		lowerThreshold = (int)max(double(0), (1 - sigma) * meanColor);
+		upperThreshold = (int)min(double(255), (1 + sigma) * meanColor);
+	}
+}
+
+/*
+* findMakers:
+*	Function takes images that have already been identified as possible books
+*		and looks for any makers on the image which could be possible titles or
+*		images seen on book covers
+*		Postconditions: image is a valid image.
+*       Postconditions: true or false of return depending on whether any makers were found.
+*/
+bool findMarkers(Mat image)
+{
+	Mat grayImage;
+	cvtColor(image, grayImage, COLOR_BGR2GRAY);
+
+	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
+
+	morphologyEx(grayImage, grayImage, MORPH_GRADIENT, element);
+	threshold(grayImage, grayImage, 0.0, 255.0, THRESH_BINARY | THRESH_OTSU);
+
+	Mat connected;
+	element = getStructuringElement(MORPH_RECT, Size(9, 1));
+	morphologyEx(grayImage, connected, MORPH_CLOSE, element);
+	
+	Mat mask = Mat::zeros(grayImage.size(), CV_8UC1);
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(connected, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	
+	for (int i = 0; i >= 0; i = hierarchy[i][0])
+	{
+		//find all the rectangle -ish contours in the image
+		Rect rect = boundingRect(contours[i]);
+		drawContours(mask, contours, i, Scalar(255, 255, 255), FILLED);
+	
+		Mat nonZero(mask, rect);
+		double ratioOfNonZeroPixel = (double)countNonZero(nonZero) / (double(rect.width) * double(rect.height));
+
+		//try to make it so that it won't be counted as an title/word unless it's at a certain
+		//size and has a certain amount of non zero pixels (text) 
+		// *These values can be adjusted 
+		if (ratioOfNonZeroPixel > .40 && (rect.height > 8 && rect.width > 8))
+		{
+			rectangle(image, rect, Scalar(0, 255, 0), 2);
+			return true;
+		}
+	}
 	return false;
 }
 
 
 int main(int argc, char* argv[])
 {
+	//Mat image = imread("bookTest.jpg");
 	Mat image = imread("bookTest.jpg");
-	//Mat image = imread("bookTest#2.jpg");
 	Mat greyImage;
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 
+	
+	
 	cvtColor(image, greyImage, COLOR_BGR2GRAY);
 	GaussianBlur(greyImage, greyImage, Size(3, 3), 0);
-	double threshold = 100;
-	Canny(greyImage, greyImage, threshold, threshold * 2);
+
+	Scalar meanColor = mean(greyImage);
+	cout << " " << meanColor[0] << endl;
+	
+	int lower = 0;
+	int upper = 0;
+	findBounds(meanColor[0], lower, upper);
+
+	//edged = cv2.Canny(gray_image, lower, upper)
+	//cv2.imshow('Edges', edged)
+
+	//threshold(greyImage, greyImage, 0, 255, THRESH_OTSU + THRESH_BINARY);
+	Canny(greyImage, greyImage, lower, upper);
+	//double threshold = 100;
+	//Canny(greyImage, greyImage, threshold, threshold * 2);
 	dilate(greyImage, greyImage, Mat(), Point(-1, -1));
+	namedWindow("Canny", WINDOW_AUTOSIZE);
 	imshow("Canny", greyImage);
+
+	
+
+
+	imshow("Canny1", greyImage);
 	findContours(greyImage, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 	
 	drawContours(image, contours, -1, Scalar(0, 255, 0), 2);
-	//imshow("con", image);
+	namedWindow("con", WINDOW_AUTOSIZE);
+	imshow("con", image);
 	int bookNumber = 0;
 	vector<Point> points;
 
-	for (int i = 0; i < contours.size(); ++i)
+	for (int i = 0; i >= 0; i = hierarchy[i][0])
 	{
-
+		
 		double area = contourArea(contours[i]);
 		cout << "area of object # " << i << " = " << area << endl;
 
-		double epsilon = .1 * arcLength(contours[i], true);
+		double epsilon = .02 * arcLength(contours[i], true);
 
 		
 		approxPolyDP(contours[i], points, epsilon, true);
 
 
-		if (points.size() == 4)
+		if (points.size() >= 4 && points.size() <= 7)
 		{
 			cout << "THIS IS A RECTANGLE, SIZE " << points.size() << endl;
 		}
@@ -170,7 +276,7 @@ int main(int argc, char* argv[])
 
 
 		
-		if (area < 600) {
+		if (area < 2000) {
 			continue;
 		}
 
@@ -191,16 +297,17 @@ int main(int argc, char* argv[])
 		}
 
 		Mat disp(image, rectangle);
-		imshow("Display", disp);
+
+		namedWindow("test", WINDOW_AUTOSIZE);
+		imshow("test", disp);
 		waitKey(0);
 
-
 		cout << endl << endl;
-		// check to see if the detected book is a solid color
-		bool sameColor = isSolidColor(disp);
-		// if so - don't mark it as a book
-		if (sameColor) {
-			continue;
+		
+		//after we have found a possible rectangle, do more checks to see
+		//if the object has any markers or is just one color.
+		if (isSolidColor(disp) || !findMarkers(disp)) {
+		    continue;
 		}
 		// otherwise
 		else {
